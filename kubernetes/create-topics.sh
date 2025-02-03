8b635535c6cd3ec17af0bc35d143c5fced9fe30f
Samirudin <61079102+Samirudin242@users.noreply.github.com>
2025-02-04 02:08:15 +0700

    update create topics

--- kubernetes/create-topics.sh
+++ kubernetes/create-topics.sh
@@ -1,6 +1,12 @@
 #local-confluent-kafka-cp-zookeeper-headless
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --delete --if-exists
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic user-created --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic product-created --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic warehouse-created --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic stock-updated --delete --if-exists
+kafka-topics --zookeeper $1:2181 --topic user-edited --delete --if-exists
 
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-request --create --partitions 3 --replication-factor 1 --if-not-exists
-kafka-topics --zookeeper $1:2181 --topic warehouse-reporting-response --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic user-created --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic product-created --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic warehouse-created --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic stock-updated  --create --partitions 3 --replication-factor 1 --if-not-exists
+kafka-topics --zookeeper $1:2181 --topic user-edited --create --partitions 3 --replication-factor 1 --if-not-exists
